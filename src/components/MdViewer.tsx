25f618a6ab4b4359030a64c0a665e7dbb9476dca
caicai <1144164505@qq.com>
2023-06-17 05:06:22 +0800

    feature: 完善Markdown预览组件

--- src/components/MdViewer.tsx
+++ src/components/MdViewer.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: caishiyin
  * @Date: 2023-06-17 03:40:47
- * @LastEditTime: 2023-06-17 04:23:51
+ * @LastEditTime: 2023-06-17 04:59:10
  * @LastEditors: caishiyin
  * @Description:
  * @FilePath: /blog-tsx/src/components/MdViewer.tsx
@@ -10,10 +10,10 @@ import React from 'react'
 import '@toast-ui/editor/dist/toastui-editor.css'
 import { Viewer } from '@toast-ui/react-editor'
 
-const MdViewer = () => {
+const MdViewer = ({ content = '' }: any) => {
     return  (
         <Viewer
-            initialValue='### hello react editor world!'
+            initialValue={content}
         />
     )
 }
